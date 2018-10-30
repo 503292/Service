f0eb4270517fe27a97844427524bf6c075d8aec6
Maxym Kalysh <503292@ukr.net>
2018-10-30 18:32:01 +0200

    Віддалений інтерфейс

--- ServiceRMI/src/net/ukr/ServiceServer.java
+++ ServiceRMI/src/net/ukr/ServiceServer.java
@@ -7,5 +7,4 @@ import java.security.Provider.Service;
 public interface ServiceServer extends Remote {
 	Object[] getServiceList() throws RemoteException;
 	Service getService(Object serviceKey) throws RemoteException;
-
 }
